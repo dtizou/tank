a6fe97fa3203f1b0c1e582996c4cb078eb7abb6d
James <james.shi@sbcglobal.net>
2016-06-29 15:13:31 -0700

    rect can't leave canvas now

--- index.js
+++ index.js
@@ -14,21 +14,33 @@ io.on('connection', function(socket){
   console.log('connected');
   socket.on('moveLeft', function(){
     x -= 10;
+    if (x <= 0) {
+      x = 0;
+    }
     io.emit('draw', [x, y]);
     console.log('moveLeft');
   });
   socket.on('moveRight', function(){
     x += 10;
+    if (x >= 1150) {
+      x = 1150;
+    }
     io.emit('draw', [x, y]);
     console.log('moveRight');
   });
   socket.on('moveUp', function(){
     y -= 10;
+    if (y <= 0) {
+      y = 0;
+    }
     io.emit('draw', [x, y]);
     console.log('moveUp');
   });
   socket.on('moveDown', function(){
     y += 10;
+    if (y >= 650) {
+      y = 650;
+    }
     io.emit('draw', [x, y]);
     console.log('moveDown');
   });
