0088a1ebd96f6bf0ce98f7d1acec2d6ccc3f921d
James <james.shi@sbcglobal.net>
2016-06-29 16:19:34 -0700

    multiple rectangles

--- index.js
+++ index.js
@@ -6,46 +6,47 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-var x = 0;
-var y = 0;
-
-//var users = {};
+var users = {};
 
 io.on('connection', function(socket){
-  io.emit('draw', [x, y]);
+  users[socket.id] = {x: 0, y: 0, width: 50, height: 50, color: '#ff0000'};
+  io.emit('draw', users);
   console.log('connected ' + socket.id);
   socket.on('moveLeft', function(){
-    x -= 10;
-    if (x <= 0) {
-      x = 0;
+    users[socket.id].x -= 10;
+    if (users[socket.id].x <= 0) {
+      users[socket.id].x = 0;
     }
-    io.emit('draw', [x, y]);
+    io.emit('draw', users);
     console.log('moveLeft ' + socket.id);
   });
   socket.on('moveRight', function(){
-    x += 10;
-    if (x >= 1150) {
-      x = 1150;
+    users[socket.id].x += 10;
+    if (users[socket.id].x >= 1150) {
+      users[socket.id].x = 1150;
     }
-    io.emit('draw', [x, y]);
+    io.emit('draw', users);
     console.log('moveRight ' + socket.id);
   });
   socket.on('moveUp', function(){
-    y -= 10;
-    if (y <= 0) {
-      y = 0;
+    users[socket.id].y -= 10;
+    if (users[socket.id].y <= 0) {
+      users[socket.id].y = 0;
     }
-    io.emit('draw', [x, y]);
+    io.emit('draw', users);
     console.log('moveUp ' + socket.id);
   });
   socket.on('moveDown', function(){
-    y += 10;
-    if (y >= 650) {
-      y = 650;
+    users[socket.id].y += 10;
+    if (users[socket.id].y >= 650) {
+      users[socket.id].y = 650;
     }
-    io.emit('draw', [x, y]);
+    io.emit('draw', users);
     console.log('moveDown ' + socket.id);
   });
+  socket.on('disconnect', function(){
+    delete users[socket.id];
+  })
 });
 
 http.listen(3000, function(){
