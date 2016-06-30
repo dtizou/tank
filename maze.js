29ec45a3b7f54a3cfe901912b87afcf7557feca4
tongzou <tong@cal.berkeley.edu>
2016-06-30 10:01:09 -0700

    added maze

---
+++ maze.js
@@ -0,0 +1,27 @@
+
+
+var stack;
+
+var Cell = function(i, j) {
+	this.i = i;
+	this.j = j;
+	this.left = true;
+	this.right = true;
+	this.top = true;
+	this.bottom = true;
+};
+
+module.exports = {
+
+	generateMaze: function(rows, cols) {
+		var maze = [], i, j;
+		for (i = 0; i < rows; i++) {
+			maze[i] = [];
+			for (j = 0; j < cols; j++) {
+				maze[i][j] = new Cell(i, j);
+			}
+		}
+
+		return maze;
+	}
+};
