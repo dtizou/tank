375f5b4cebf99ad278873b54429bc7545222f4a8
James <james.shi@sbcglobal.net>
2016-06-30 15:38:28 -0700

    finished maze

--- index.js
+++ index.js
@@ -1,56 +1,73 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var maze = require('./maze');
+var mazeGen = require('./maze');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 var users = {};
+var canvasSize = [700, 1200]; //height and width
+var mazeDimensions = [7, 12]; //rows and cols
+var maze = mazeGen.generateMaze(mazeDimensions[0], mazeDimensions[1]);
+var speed = 10;
 
 function drawTanks() {
   io.emit('drawTanks', users);
 }
 
 function drawMaze() {
-  io.emit('drawMaze', maze.generateMaze(7, 12));
+  io.emit('drawMaze', maze);
+}
+
+function setCanvasSize() {
+  io.emit('setCanvasSize', canvasSize);
+}
+
+function fixCollision(socketid) {
+  var cell = [Math.floor((users[socketid].y + users[socketid].height / 2) * mazeDimensions[0] / canvasSize[0]), Math.floor((users[socketid].x + users[socketid].width / 2) * mazeDimensions[1] / canvasSize[1])];
+  if (users[socketid].x < cell[1] * canvasSize[1] / mazeDimensions[1] && maze.maze[cell[0]][cell[1]].left) {
+    users[socketid].x = cell[1] * canvasSize[1] / mazeDimensions[1];
+  }
+  if (users[socketid].x > (cell[1] + 1) * canvasSize[1] / mazeDimensions[1] - users[socketid].width && maze.maze[cell[0]][cell[1]].right) {
+    users[socketid].x = (cell[1] + 1) * canvasSize[1] / mazeDimensions[1] - users[socketid].width;
+  }
+  if (users[socketid].y < cell[0] * canvasSize[0] / mazeDimensions[0] && maze.maze[cell[0]][cell[1]].top) {
+    users[socketid].y = cell[0] * canvasSize[0] / mazeDimensions[0];
+  }
+  if (users[socketid].y > (cell[0] + 1) * canvasSize[0] / mazeDimensions[0] - users[socketid].height && maze.maze[cell[0]][cell[1]].bottom) {
+    users[socketid].y = (cell[0] + 1) * canvasSize[0] / mazeDimensions[0] - users[socketid].height;
+  }
 }
 
 io.on('connection', function(socket){
   //Things to do when connection to socket starts
   users[socket.id] = {x: 0, y: 0, width: 50, height: 50, color: '#ff0000'};
+  setCanvasSize();
   drawMaze();
   drawTanks();
   //console.log(socket.id);
   
   //Receiving events
   socket.on('moveLeft', function(){
-    users[socket.id].x -= 10;
-    if (users[socket.id].x <= 0) {
-      users[socket.id].x = 0;
-    }
+    users[socket.id].x -= speed;
+    fixCollision(socket.id);
     drawTanks();
   });
   socket.on('moveRight', function(){
-    users[socket.id].x += 10;
-    if (users[socket.id].x >= 1150) {
-      users[socket.id].x = 1150;
-    }
+    users[socket.id].x += speed;
+    fixCollision(socket.id);
     drawTanks();
   });
   socket.on('moveUp', function(){
-    users[socket.id].y -= 10;
-    if (users[socket.id].y <= 0) {
-      users[socket.id].y = 0;
-    }
+    users[socket.id].y -= speed;
+    fixCollision(socket.id);
     drawTanks();
   });
   socket.on('moveDown', function(){
-    users[socket.id].y += 10;
-    if (users[socket.id].y >= 650) {
-      users[socket.id].y = 650;
-    }
+    users[socket.id].y += speed;
+    fixCollision(socket.id);
     drawTanks();
   });
   
--- maze.js
+++ maze.js
@@ -1,7 +1,5 @@
 
 
-var stack;
-
 var Cell = function(i, j) {
 	this.i = i;
 	this.j = j;
@@ -9,19 +7,66 @@ var Cell = function(i, j) {
 	this.right = true;
 	this.top = true;
 	this.bottom = true;
+	this.visited = false;
 };
 
 module.exports = {
-
 	generateMaze: function(rows, cols) {
-		var maze = [], i, j;
+		var maze = [], stack = [], curCell, nextCell, cellList, i, j;
 		for (i = 0; i < rows; i++) {
 			maze[i] = [];
 			for (j = 0; j < cols; j++) {
 				maze[i][j] = new Cell(i, j);
 			}
 		}
+		i = Math.floor(Math.random() * rows);
+		j = Math.floor(Math.random() * cols);
+		stack.push([i, j]);
+		maze[i][j].visited = true;
+		for (i = 0; i < rows * cols - 1; i++) {
+			curCell = stack[stack.length - 1];
+			while ((cellList = this.validCells(maze, rows, cols, curCell[0], curCell[1])).length == 0) {
+				stack.pop();
+				curCell = stack[stack.length - 1];
+			}
+			nextCell = cellList[Math.floor(Math.random() * cellList.length)];
+			if (curCell[0] + 1 == nextCell[0]) {
+				maze[curCell[0]][curCell[1]].bottom = false;
+				maze[nextCell[0]][nextCell[1]].top = false;
+			}
+			else if (curCell[0] - 1 == nextCell[0]) {
+				maze[curCell[0]][curCell[1]].top = false;
+				maze[nextCell[0]][nextCell[1]].bottom = false;
+			}
+			else if (curCell[1] + 1 == nextCell[1]) {
+				maze[curCell[0]][curCell[1]].right = false;
+				maze[nextCell[0]][nextCell[1]].left = false;
+			}
+			else if (curCell[1] - 1 == nextCell[1]) {
+				maze[curCell[0]][curCell[1]].left = false;
+				maze[nextCell[0]][nextCell[1]].right = false;
+			}
+			maze[nextCell[0]][nextCell[1]].visited = true;
+			stack.push(nextCell);
+		}
 
 		return {maze: maze, rows: rows, cols: cols};
+	},
+
+	validCells: function(maze, rows, cols, x, y) {
+		var cells = [];
+		if (y >= 1 && !maze[x][y-1].visited) {
+			cells.push([x, y-1]);
+		}
+		if (y < cols-1 && !maze[x][y+1].visited) {
+			cells.push([x, y+1]);
+		}
+		if (x >= 1 && !maze[x-1][y].visited) {
+			cells.push([x-1, y]);
+		}
+		if (x < rows-1 && !maze[x+1][y].visited) {
+			cells.push([x+1, y]);
+		}
+		return cells;
 	}
 };
